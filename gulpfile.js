c004800f5303ddb53067857819a25e0eb9817de2
Arseniy Svyatov <cuznetsovsenya@yandex.ru>
2023-10-03 23:17:18 +0300

    первый коммит

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat-css');
+const plumber = require('gulp-plumber');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const mediaquery = require('postcss-combine-media-query');
+const cssnano = require('cssnano');
+const htmlMinify = require('html-minifier');
+
+function serve() {
+    browserSync.init({
+      server: {
+        baseDir: './dist'
+      }
+    });
+  }
+
+  function html() {
+    const options = {
+      removeComments: true,
+      removeRedundantAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      sortClassName: true,
+      useShortDoctype: true,
+      collapseWhitespace: true,
+        minifyCSS: true,
+        keepClosingSlash: true
+    };
+  return gulp.src('src/**/*.html')
+        .pipe(plumber())
+                .on('data', function(file) {
+              const buferFile = Buffer.from(htmlMinify.minify(file.contents.toString(), options))
+              return file.contents = buferFile
+            })
+                .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.reload({stream: true}));
+}
+
+function css() {
+    const plugins = [
+         autoprefixer(),
+         mediaquery(),
+         cssnano()
+    ];
+  return gulp.src('src/blocks/**/*.css')
+        .pipe(plumber())
+        .pipe(concat('bundle.css'))
+        .pipe(postcss(plugins))
+                .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.reload({stream: true}));
+}
+  
+  function images() {
+    return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
+              .pipe(gulp.dest('dist/images'))
+              .pipe(browserSync.reload({stream: true}));
+  }
+
+  function scripts() {
+    return gulp.src('src/scripts/**/*.js')
+            .pipe(gulp.dest('dist/scripts'))
+            .pipe(browserSync.reload({stream: true}));
+  }
+
+  function fonts() {
+    return gulp.src('src/fonts/**/*.*')
+            .pipe(gulp.dest('dist/fonts'))
+            .pipe(browserSync.reload({stream: true}));
+  }
+
+  function clean() {
+    return del('dist');
+  }
+
+  function watchFiles() {
+    gulp.watch(['src/**/*.html'], html);
+    gulp.watch(['src/blocks/**/*.css'], css);
+    gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  }
+
+  const build = gulp.series(clean, gulp.parallel(html, css, images, scripts, fonts));
+  const watchapp = gulp.parallel(build, watchFiles, serve);
+
+
+  exports.clean = clean;
+  exports.images = images;
+  exports.css = css;
+  exports.html = html;
+  
+  exports.build = build;
+  exports.watchapp = watchapp;
+  exports.default = watchapp;
